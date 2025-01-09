9c9630b484998c30962e68da20adcc2633e90a61
aswin1661 <aswinsanthoshachus@gmail.com>
2025-01-09 20:57:15 +0530

    Updates

---
+++ precache-manifest.12dbffbd324e85369f21f326cb70ec2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c10ac59d4b7747cc7a9e46cef4470f2",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d0a6cdd5b939f71ee740",
+    "url": "/react-app/static/css/main.e51ea9cd.chunk.css"
+  },
+  {
+    "revision": "e96de8ce9ca193938b87",
+    "url": "/react-app/static/js/2.9da0c732.chunk.js"
+  },
+  {
+    "revision": "d0a6cdd5b939f71ee740",
+    "url": "/react-app/static/js/main.1f93ae44.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
